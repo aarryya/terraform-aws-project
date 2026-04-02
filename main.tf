6d3e0ce667f24615f4f1368e9a16c6df674924be
aarryya <aarya11121@gmail.com>
2026-04-02 21:49:30 +0530

    Final clean commit - removed secrets

---
+++ main.tf
@@ -0,0 +1,186 @@
+# ---------------- VPC ----------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# ---------------- SUBNET 1 ----------------
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+# ---------------- SUBNET 2 ----------------
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+# ---------------- INTERNET GATEWAY ----------------
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# ---------------- ROUTE TABLE ----------------
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-rt"
+  }
+}
+
+# ---------------- ROUTE ----------------
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+# ---------------- ROUTE ASSOCIATIONS ----------------
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# ---------------- SECURITY GROUP ----------------
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# ---------------- ECS CLUSTER ----------------
+resource "aws_ecs_cluster" "main" {
+  name = "my-ecs-cluster"
+}
+
+# ---------------- TASK DEFINITION ----------------
+resource "aws_ecs_task_definition" "app" {
+  family                   = "app-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = "nginx"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+# ---------------- LOAD BALANCER ----------------
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  load_balancer_type = "application"
+  subnets = [
+    aws_subnet.public.id,
+    aws_subnet.public2.id
+  ]
+  security_groups = [aws_security_group.web_sg.id]
+}
+
+# ---------------- TARGET GROUP ----------------
+resource "aws_lb_target_group" "tg" {
+  name        = "app-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+}
+
+# ---------------- LISTENER ----------------
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+# ---------------- ECS SERVICE ----------------
+resource "aws_ecs_service" "app" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets = [
+      aws_subnet.public.id,
+      aws_subnet.public2.id
+    ]
+    security_groups  = [aws_security_group.web_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "app"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
